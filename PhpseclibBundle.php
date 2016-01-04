d4e7249c5700f8a05461c924d5108182d0da73d9
Mark Spilsbury <mark.spilsbury@madebypi.co.uk>
2016-01-04 15:15:04 +0000

    Move files into more standard bundle structure

--- PhpseclibBundle.php
+++ PhpseclibBundle.php
@@ -2,25 +2,6 @@
 
 namespace Sinner\Phpseclib;
 
-require_once(__DIR__.'/Crypt/AES.php');
-require_once(__DIR__.'/Crypt/DES.php');
-require_once(__DIR__.'/Crypt/Hash.php');
-require_once(__DIR__.'/Crypt/RC4.php');
-require_once(__DIR__.'/Crypt/Random.php');
-require_once(__DIR__.'/Crypt/Rijndael.php');
-require_once(__DIR__.'/Crypt/RSA.php');
-require_once(__DIR__.'/Crypt/TripleDES.php');
-
-require_once(__DIR__.'/File/ANSI.php');
-require_once(__DIR__.'/File/ASN1.php');
-require_once(__DIR__.'/File/X509.php');
-
-require_once(__DIR__.'/Math/BigInteger.php');
-
-require_once(__DIR__.'/Net/SFTP.php');
-require_once(__DIR__.'/Net/SSH1.php');
-require_once(__DIR__.'/Net/SSH2.php');
-
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 
@@ -43,12 +24,10 @@ use Sinner\Phpseclib\Net\Net_SSH1 as SSH1;
 use Sinner\Phpseclib\Net\Net_SSH2 as SSH2;
 
 
-class PhpseclibBundle extends Bundle{
-
-	public function build(ContainerBuilder $container){
-		
+class PhpseclibBundle extends Bundle
+{
+	public function build(ContainerBuilder $container)
+    {
 		parent::build($container);
-
 	}
-
 }
